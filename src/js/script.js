219c264021f16c9e919e3b0dd18645a8f15c8958
Tetiana Bronitska <tetiana.bron@gmail.com>
2021-11-27 02:57:50 +0200

    Added skills

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,11 @@
 const refs = {
+  //Hamdurger menu
   hamburger: document.querySelector('.hamburger'),
   menu: document.querySelector('.menu'),
   menuVBtnClose: document.querySelector('.menu__btn-close'),
+  //Counting percent in skill section
+  counters: document.querySelectorAll('.skills__rating-item-counter'),
+  lines: document.querySelectorAll('.skills__rating-item-line span'),
 };
 
 refs.hamburger.addEventListener('click', () => {
@@ -9,4 +13,8 @@ refs.hamburger.addEventListener('click', () => {
 });
 refs.menuVBtnClose.addEventListener('click', () => {
   refs.menu.classList.remove('active');
+});
+
+refs.counters.forEach((item, i) => {
+  refs.lines[i].style.width = item.textContent;
 });
